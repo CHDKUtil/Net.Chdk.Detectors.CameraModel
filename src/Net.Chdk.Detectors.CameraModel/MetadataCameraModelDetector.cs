94ee59a73a76944eab69469e53c6359512c36468
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 17:00:55 +0300

    Add solution

---
+++ src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
@@ -0,0 +1,22 @@
+﻿using Net.Chdk.Detectors.CameraModel.Core;
+using Net.Chdk.Model.CameraModel;
+using Net.Chdk.Model.Card;
+using Microsoft.Extensions.Logging;
+
+namespace Net.Chdk.Detectors.CameraModel
+{
+    public sealed class MetadataCameraModelDetector : MetadataDetector<MetadataCameraModelDetector, CameraModelInfo>, ICameraModelDetector
+    {
+        public MetadataCameraModelDetector(ILoggerFactory loggerFactory)
+            : base(loggerFactory)
+        {
+        }
+
+        public CameraModelInfo GetCameraModel(CardInfo cardInfo)
+        {
+            return GetValue(cardInfo);
+        }
+
+        protected override string FileName => "MODEL.JSN";
+    }
+}
