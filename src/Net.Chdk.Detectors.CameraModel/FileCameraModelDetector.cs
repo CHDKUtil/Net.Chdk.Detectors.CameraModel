a38352ca70db47ba0c604aa49576c9e90d01f531
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 20:45:03 +0300

    Add FileCameraModelDetector

---
+++ src/Net.Chdk.Detectors.CameraModel/FileCameraModelDetector.cs
@@ -0,0 +1,26 @@
+﻿using Net.Chdk.Detectors.Camera;
+using Net.Chdk.Model.CameraModel;
+using Net.Chdk.Providers.CameraModel;
+
+namespace Net.Chdk.Detectors.CameraModel
+{
+    public sealed class FileCameraModelDetector : IFileCameraModelDetector
+    {
+        private IFileCameraDetector FileCameraDetector { get; }
+        private ICameraModelProvider CameraModelProvider { get; }
+
+        public FileCameraModelDetector(IFileCameraDetector fileCameraDetector, ICameraModelProvider cameraModelProvider)
+        {
+            FileCameraDetector = fileCameraDetector;
+            CameraModelProvider = cameraModelProvider;
+        }
+
+        public CameraModelInfo[] GetCameraModels(string filePath)
+        {
+            var cameraInfo = FileCameraDetector.GetCamera(filePath);
+            if (cameraInfo == null)
+                return null;
+            return CameraModelProvider.GetCameraModels(cameraInfo);
+        }
+    }
+}
