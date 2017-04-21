ac7c2fa9140b2cfc4af927cf7e2648379f9f2582
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 18:19:39 +0300

    Encapsulate CameraModelDetector

---
+++ src/Net.Chdk.Detectors.CameraModel/CameraModelDetector.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Net.Chdk.Model.CameraModel;
+using Net.Chdk.Model.Card;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net.Chdk.Detectors.CameraModel
+{
+    public sealed class CameraModelDetector : ICameraModelDetector
+    {
+        private ILoggerFactory LoggerFactory { get; }
+        private IEnumerable<ICameraModelDetector> CameraModelDetectors { get; }
+
+        public CameraModelDetector(ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory;
+            CameraModelDetectors = new ICameraModelDetector[]
+            {
+                new MetadataCameraModelDetector(LoggerFactory),
+            };
+        }
+
+        public CameraModelInfo GetCameraModel(CardInfo cardInfo)
+        {
+            return CameraModelDetectors
+                .Select(d => d.GetCameraModel(cardInfo))
+                .FirstOrDefault(c => c != null);
+        }
+    }
+}
--- src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
+++ src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Net.Chdk.Detectors.CameraModel
 {
-    public sealed class MetadataCameraModelDetector : MetadataDetector<MetadataCameraModelDetector, CameraModelInfo>, ICameraModelDetector
+    sealed class MetadataCameraModelDetector : MetadataDetector<MetadataCameraModelDetector, CameraModelInfo>, ICameraModelDetector
     {
         public MetadataCameraModelDetector(ILoggerFactory loggerFactory)
             : base(loggerFactory)
