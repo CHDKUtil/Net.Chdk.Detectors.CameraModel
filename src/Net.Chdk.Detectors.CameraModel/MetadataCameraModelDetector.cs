068285a96f986c9373c6887c7e7ff538e00c0c36
Dmitry Shechtman <damnian@gmail.com>
2017-04-24 17:15:53 +0300

    Add validation

--- src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
+++ src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
@@ -2,6 +2,8 @@
 using Net.Chdk.Model.Camera;
 using Net.Chdk.Model.CameraModel;
 using Net.Chdk.Model.Card;
+using System;
+using System.Linq;
 
 namespace Net.Chdk.Detectors.CameraModel
 {
@@ -17,11 +19,44 @@ namespace Net.Chdk.Detectors.CameraModel
             Logger.LogTrace("Detecting camera models from {0} metadata", cardInfo.DriveLetter);
 
             var modelInfo = GetValue(cardInfo);
-            return modelInfo != null
-                ? new[] { modelInfo }
-                : null;
+            if (modelInfo == null)
+                return null;
+
+            if (!ValidateVersion(modelInfo))
+                return null;
+
+            if (!ValidateNames(modelInfo))
+                return null;
+
+            return new[] { modelInfo };
         }
 
         protected override string FileName => "MODEL.JSN";
+
+        private static bool ValidateVersion(CameraModelInfo modelInfo)
+        {
+            Version version;
+            if (!Version.TryParse(modelInfo.Version, out version))
+                return false;
+
+            if (version.Major < 1 || version.Minor < 0)
+                return false;
+
+            return true;
+        }
+
+        private static bool ValidateNames(CameraModelInfo modelInfo)
+        {
+            if (modelInfo.Names == null)
+                return false;
+
+            if (modelInfo.Names.Length == 0)
+                return false;
+
+            if (modelInfo.Names.Any(string.IsNullOrEmpty))
+                return false;
+
+            return true;
+        }
     }
 }
