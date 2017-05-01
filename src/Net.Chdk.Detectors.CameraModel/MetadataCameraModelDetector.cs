59f3f67a2552f4e9066140120e0502251b0dedfd
Dmitry Shechtman <damnian@gmail.com>
2017-05-02 02:58:58 +0300

    Extract validator

--- src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
+++ src/Net.Chdk.Detectors.CameraModel/MetadataCameraModelDetector.cs
@@ -2,15 +2,14 @@
 using Net.Chdk.Model.Camera;
 using Net.Chdk.Model.CameraModel;
 using Net.Chdk.Model.Card;
-using System;
-using System.Linq;
+using Net.Chdk.Validators;
 
 namespace Net.Chdk.Detectors.CameraModel
 {
     sealed class MetadataCameraModelDetector : MetadataDetector<MetadataCameraModelDetector, CameraModelInfo>, IInnerCameraModelDetector
     {
-        public MetadataCameraModelDetector(ILoggerFactory loggerFactory)
-            : base(loggerFactory)
+        public MetadataCameraModelDetector(IValidator<CameraModelInfo> validator, ILoggerFactory loggerFactory)
+            : base(validator, loggerFactory)
         {
         }
 
@@ -22,40 +21,9 @@ namespace Net.Chdk.Detectors.CameraModel
             if (cameraModel == null)
                 return null;
 
-            if (!Validate(cameraModel.Version))
-                return null;
-
-            if (!Validate(cameraModel.Names))
-                return null;
-
             return new[] { cameraModel };
         }
 
         protected override string FileName => "MODEL.JSN";
-
-        private static bool Validate(Version version)
-        {
-            if (version == null)
-                return false;
-
-            if (version.Major < 1 || version.Minor < 0)
-                return false;
-
-            return true;
-        }
-
-        private static bool Validate(string[] names)
-        {
-            if (names == null)
-                return false;
-
-            if (names.Length == 0)
-                return false;
-
-            if (names.Any(string.IsNullOrEmpty))
-                return false;
-
-            return true;
-        }
     }
 }
